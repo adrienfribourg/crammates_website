b7b8eaff42ebe3485b03b3ed731b36e1bd5f7511
adrienfribourg <afribourg1503@gmail.com>
2024-09-11 00:17:26 +0100

    Fix DATABASE_URL to use postgresql dialect

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = '542bed3a43a4a42ec1a5cbdbfac2d9015343bfb3f885061c'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    
+    # Fix the DATABASE_URL by replacing 'postgres://' with 'postgresql://'
+    database_url = os.environ.get('DATABASE_URL')
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://")
+
+    SQLALCHEMY_DATABASE_URI = database_url or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
