f4ed25e1808d2401c6055fff92659c93667300c5
adrienfribourg <afribourg1503@gmail.com>
2024-09-12 00:22:30 +0100

    Merged study_session and c1a4fb2a2429 heads

---
+++ migrations/versions/4d1ffe7bc01a_merge_study_session_and_c1a4fb2a2429_.py
@@ -0,0 +1,24 @@
+"""Merge study_session and c1a4fb2a2429 heads
+
+Revision ID: 4d1ffe7bc01a
+Revises: 
+Create Date: 2024-09-12 00:21:32.091486
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4d1ffe7bc01a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
