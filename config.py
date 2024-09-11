f7e2e30616a4d6c3e9160a4b240e12fc87c53557
adrienfribourg <afribourg1503@gmail.com>
2024-09-11 23:33:21 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = '542bed3a43a4a42ec1a5cbdbfac2d9015343bfb3f885061c'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    # Update DATABASE_URL if it starts with postgres://
+    uri = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
