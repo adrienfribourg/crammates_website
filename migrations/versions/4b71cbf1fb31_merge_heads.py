b7c09a29ba05ed247a89bf509ad6a7508f8049c8
adrienfribourg <afribourg1503@gmail.com>
2024-09-11 23:49:11 +0100

    Merged heads

---
+++ migrations/versions/4b71cbf1fb31_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: 4b71cbf1fb31
+Revises: 
+Create Date: 2024-09-11 23:47:27.473787
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4b71cbf1fb31'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
