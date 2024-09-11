f43271e031a2060695950881391b87500ad9aa44
adrienfribourg <afribourg1503@gmail.com>
2024-09-12 00:24:30 +0100

    Merged all heads into one

---
+++ migrations/versions/f5b00c0ba740_merge_all_heads_into_one.py
@@ -0,0 +1,24 @@
+"""Merge all heads into one
+
+Revision ID: f5b00c0ba740
+Revises: 
+Create Date: 2024-09-12 00:24:21.713909
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f5b00c0ba740'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
