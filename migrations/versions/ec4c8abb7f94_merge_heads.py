df3697b68230d230a8720ae434bff7f1c68f5e5f
adrienfribourg <afribourg1503@gmail.com>
2024-09-13 17:50:25 +0100

    Finalize migrations and resolve local issues

---
+++ migrations/versions/ec4c8abb7f94_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: ec4c8abb7f94
+Revises: d7f8e2c9a3b4, 01b6cceba8a0
+Create Date: 2024-09-13 17:37:02.511666
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ec4c8abb7f94'
+down_revision = ('d7f8e2c9a3b4', '01b6cceba8a0')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
